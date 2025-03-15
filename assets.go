801848f3174e8b7dee2cadc561bbce2217f4cdc2
lancej1022 <46359098+lancej1022@users.noreply.github.com>
2025-03-14 18:50:24 -0700

    Replace signed urls with cloudfront cdn serving

--- assets.go
+++ assets.go
@@ -27,10 +27,6 @@ func getAssetPath(mediaType string) string {
 	return fmt.Sprintf("%s%s", fileName, ext)
 }
 
-func (cfg apiConfig) getObjectURL(key string) string {
-	return fmt.Sprintf("https://%s.s3.%s.amazonaws.com/%s", cfg.s3Bucket, cfg.s3Region, key)
-}
-
 func (cfg apiConfig) getAssetDiskPath(assetPath string) string {
 	return filepath.Join(cfg.assetsRoot, assetPath)
 }
